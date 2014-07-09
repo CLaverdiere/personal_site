38dc24d22f1a1bace7394de2c710f317d19bbd3a
Chris Laverdiere <cmlaverdiere@gmail.com>
2014-07-08 21:49:26 -0400

    Update dynamic book links, prog links, theming, templates, 404 page.

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+# App settings
+app.config.from_object(__name__)
+app.config.update(dict(
+    DEBUG=True,
+))
+
 @app.route('/')
 def root():
-    return render_template('index.html')
+    with open("static/content/books.txt") as bookfile:
+        books = bookfile.readlines()
+    return render_template('index.html', books=books)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
+    app.DEBUG = True
     app.run()
